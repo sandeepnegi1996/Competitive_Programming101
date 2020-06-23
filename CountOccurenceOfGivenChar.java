37d1b0d68b8cca03df42d049bf52982941391aa6
sandeepnegi1996 <9041128179sandy@gmail.com>
2020-06-23 11:37:04 +0530

    finished implementing the occurence of character in a string method

---
+++ CountOccurenceOfGivenChar.java
@@ -0,0 +1,44 @@
+/**
+ * CountOccurenceOfGivenChar
+ */
+public class CountOccurenceOfGivenChar {
+
+    public static int countOccurenceGivenChar(String str,char ch) {
+
+        // using n time 
+        // and constant Space
+        int count=0;
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i)==ch) {
+                count++;
+            }
+            
+        }
+
+        return count;
+
+    }
+
+    //using compare method
+    public static int countOccurenceGivenChar1(String str,char ch) {
+        // Compare will subtract the first character from the second character and returns the value
+        // so char1 == char2 -->0
+        //char1 > char2 --> +ve
+        //char1 < char2 --> -ve
+        int count=0;
+        for (int i = 0; i < str.length(); i++) 
+            if (Character.compare(str.charAt(i), ch)==0) 
+                count++;
+
+        return count;
+
+    }
+    public static void main(String[] args) {
+        
+        int result=countOccurenceGivenChar1("nnn",'n');
+        System.out.println(result);
+    }
+
+    
+}
---
+++ TestRough/TestCompare.java
@@ -0,0 +1,13 @@
+ class TestCompare {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        char char1='A';
+        char char2='C';
+
+        // comapare will do the difference of first char from the second char
+        
+        System.out.println(Character.compare(char1, char2));
+    }
+    
+}
